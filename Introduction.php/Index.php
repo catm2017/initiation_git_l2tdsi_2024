93fd960511784fee080491eced77ee59063ceb7a
03AlAmine <mentalisum20@gmail.com>
2024-03-21 11:31:00 +0000

    My TP by Wamp

---
+++ Introduction.php/Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello. <br> Mouhamad Al Amine Ba <br> Je suis sur la branche MAAB <br> Thanks";
+    phpinfo();
+    ?>
+</body>
+
+</html>
