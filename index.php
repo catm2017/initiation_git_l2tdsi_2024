351abfc7e850aeede95d50c8762f4b735c50e48c
DiawMoustapha <mamadoumoustaphadiaw44@gmail.com>
2024-03-22 17:34:51 +0000

    le capture d'ecran et le code php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ech, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello L2TDSI: je suis Mamadou Moustapha DIAW.<br> Mom usermane est Diaw_moustapha <br> Et ma branche est aussi Diaw_moustapha";
+    phpinfo();
+    ?>
+</body>
+</html>
