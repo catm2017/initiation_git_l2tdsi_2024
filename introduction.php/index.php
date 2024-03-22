8e9daa7ed1ad55d5e579913ccef43295187dd2f3
Laremad <ndeyemaremediongue7@gmail.com>
2024-03-22 17:03:26 +0100

    I am edited

---
+++ introduction.php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello World! <br> I'm Ndeye Mareme Diongue <br> My branch is Laremad ";
+    phpinfo();
+
+    ?>
+</body>
+
+</html>
