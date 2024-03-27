1692800c201769efab9817fc83f1ea6b181e4839
RokhyatouSF <rokhyatousarrfall@gmail.com>
2024-03-27 19:32:41 +0000

    presentation nom, username, versionPHP

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hello L2TDSI: Je suis <strong>Rokhyatou Sarr Fall</strong>,<br>
+Mon username est <strong>RokhyatouSF</strong> et <br>
+Ma branche est <strong>Rokhyatou</strong>";
+phpinfo()
+?>
